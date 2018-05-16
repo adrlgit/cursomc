f6a8c001686a59f464f97f4492aaf4b529f0f73f
André Luis <luis.alms91@gmail.com>
2018-05-16 00:22:12 -0300

    Estado e Cidade

---
+++ src/main/java/com/luissilva/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.luissilva.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luissilva.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+
+}
